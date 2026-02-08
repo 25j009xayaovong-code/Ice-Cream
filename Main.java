1f027ce0f2d73a66f84d1da27968b29073b22dab
Chengzong-Rin <tsheejzoovaj2002@gmail.com>
2026-02-08 11:12:39 +0900

    add encryption

---
+++ Main.java
@@ -0,0 +1,32 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(encryption("i love you 300"));
+        System.out.println(decryption(encryption("i love you 300")));
+
+    }   
+
+    public static String encryption(String text) {
+        String mergeText = "";
+        for (char c : text.toCharArray()) {
+            int t = c;
+            String tTostr = Integer.toString(t);
+            mergeText += tTostr;
+        }
+
+        return mergeText;
+    }
+
+    public static String decryption(String text) {
+        String mergeText = "";
+        for (char c : text.toCharArray()) {
+            String str = String.valueOf((char) c);
+            mergeText += str;
+        }
+
+        return mergeText;
+    }
+
+}
