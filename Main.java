18dad23f7da0b6d1ae6fecd8e97976e3d2507659
Chengzong-Rin <tsheejzoovaj2002@gmail.com>
2026-02-08 11:15:39 +0900

    add double

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(encryption("i love you 300"));
+        System.out.println(encryption(decryption(encryption("i love you 300"))));
 
     }   
 
